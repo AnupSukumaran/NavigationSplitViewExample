3064a7ee6c87560b7fe5ad11417a05961224409d
AnupSukumaran <anup.sukumaran9@gmail.com>
2025-11-12 13:55:13 +0000

    Initial Commit

---
+++ NavigationSplitViewExample/NavigationSplitViewExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationSplitViewExampleApp.swift
+//  NavigationSplitViewExample
+//
+//  Created by Sukumar.Sukumaran on 12/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct NavigationSplitViewExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
